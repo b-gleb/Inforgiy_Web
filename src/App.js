510740f350eaaf96a32d009129dbc7b4696b1865
b-gleb <lion.zz@mail.ru>
2024-12-20 12:06:42 +0000

    Add `WebApp.BackButton`, closes #12

--- src/App.js
+++ src/App.js
@@ -123,10 +123,8 @@ function UserSearchPopUp({
   mode,
   branch,
   initDataUnsafe,
-
   date,
   timeRange,
-  
   onClose,
   handleUpdateRota,
 }) {
@@ -136,6 +134,19 @@ function UserSearchPopUp({
   const [searchQuery, setSearchQuery] = useState("");
   // States for user management
   const [editingUser, setEditingUser] = useState(null);
+
+  // Telegram UI Back Button
+  useEffect(() => {
+    window.Telegram.WebApp.BackButton.onClick(onClose);
+    window.Telegram.WebApp.BackButton.show();
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.Telegram.WebApp.BackButton.offClick();
+      window.Telegram.WebApp.BackButton.hide();
+    };
+  
+  }, [onClose]);
 
   // Fetch all users
   useEffect(() => {
