1f0e36d007e6213a45a747ba1af3c316a4bb15e8
b-gleb <lion.zz@mail.ru>
2025-07-26 20:26:39 +0100

    Add React Router and minor improvements

--- assets/fetchAllUsers-CgEL_J9V.js
+++
@@ -1,1 +0,0 @@
-import{a as e,b as t}from"./index-Bo4-0vWi.js";async function c(s,a){try{return(await e.get("/api/users",{params:{branch:s,initDataUnsafe:a}})).data}catch(r){throw t(r),r}}export{c as f};
