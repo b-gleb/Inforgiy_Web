6efd8b2560e7b7fbd2ce1bcfc5a0c2be890620d0
b-gleb <lion.zz@mail.ru>
2026-01-01 13:07:44 +0000

    feat: install react-query

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    }
+  }
+});
 
 const rootElement = document.getElementById('root');
 rootElement.classList.add('bg-gray-100', 'dark:bg-neutral-900');
@@ -9,8 +18,10 @@ rootElement.classList.add('bg-gray-100', 'dark:bg-neutral-900');
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
