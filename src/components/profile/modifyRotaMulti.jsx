98ea01a7eff8ab1bf9068817b9a7800415a895a9
b-gleb <lion.zz@mail.ru>
2026-01-03 17:01:44 +0000

    refactor: `modifyRotaMulti` remove toast css

--- src/components/profile/modifyRotaMulti.jsx
+++ src/components/profile/modifyRotaMulti.jsx
@@ -1,7 +1,6 @@
 import { useState} from 'react';
 import api from '@/services/api.ts';
 import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import { eachDayOfInterval } from 'date-fns';
 import { ru } from "date-fns/locale";
 import { Button } from "@/components/ui/button";
