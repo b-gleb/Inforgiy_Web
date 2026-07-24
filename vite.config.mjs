84cde71533a9f61c08ddeb474ff48456e91909ac
b-gleb <gleb.butyagin@gmail.com>
2026-07-24 23:15:50 +0100

    feat: add storybook and vitest

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 import { fileURLToPath } from 'url';
 import { visualizer } from 'rollup-plugin-visualizer';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -16,18 +21,43 @@ export default defineConfig({
     visualizer({
       filename: 'dist_stats/treemap.html',
       template: 'treemap',
-      gzipSize: true,
+      gzipSize: true
     }),
     visualizer({
       filename: 'dist_stats/flamegraph.html',
       template: 'flamegraph',
       open: true,
-      gzipSize: true,
-    })
-  ],
+      gzipSize: true
+    })],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+      "@": path.resolve(__dirname, "./src")
+    }
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/config/test-setup.js',
+    // For tests with storybook
+    // projects: [{
+    //   extends: true,
+    //   plugins: [
+    //   // The plugin will run tests for the stories defined in your Storybook config
+    //   // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+    //   storybookTest({
+    //     configDir: path.join(dirname, '.storybook')
+    //   })],
+    //   test: {
+    //     name: 'storybook',
+    //     browser: {
+    //       enabled: true,
+    //       headless: true,
+    //       provider: playwright({}),
+    //       instances: [{
+    //         browser: 'chromium'
+    //       }]
+    //     }
+    //   }
+    // }]
   }
-})
+});
