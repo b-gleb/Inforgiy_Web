759b41d15b8d7268c1c2ddf4109f3b759ff86dca
b-gleb <gleb.butyagin@gmail.com>
2026-09-06 15:01:03 +0100

    refactor: define `UserDutiesContainerProps` as an interface

--- src/pages/rota/components/UserDuties/UserDutiesContainer.tsx
+++ src/pages/rota/components/UserDuties/UserDutiesContainer.tsx
@@ -1,10 +1,19 @@
 import UserDuties from "./UserDuties";
 import { useUserDuties } from "@/hooks/rotaHooks";
 
-export default function UserDutiesContainer({branch, userId, prevDays, nextDays}: {branch: string, userId: number, prevDays: number, nextDays: number}) {
-  const {status, data} = useUserDuties({branch, userId, prevDays, nextDays});
+export interface UserDutiesContainerProps {
+  branch: string;
+  userId: number;
+  prevDays: number;
+  nextDays: number;
+}
 
-  return (
-    <UserDuties status={status} data={data}/>
-  )
-};
+export default function UserDutiesContainer({
+  branch,
+  userId,
+  prevDays,
+  nextDays,
+}: UserDutiesContainerProps) {
+  const { status, data } = useUserDuties({ branch, userId, prevDays, nextDays });
+  return <UserDuties status={status} data={data} />;
+}
